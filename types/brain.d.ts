5f5426839fd7ff8090532da376fbbf73bce2bd69
bhandekunal16 <bhandekunal16@gmail.com>
2024-04-22 16:36:03 +0530

    Update package version Delete phone.json Type definitions for Brain class methods updated to accept string for collection parameter

--- types/brain.d.ts
+++ types/brain.d.ts
@@ -1,15 +1,18 @@
 export = Brain;
 declare class Brain {
-    write(input: any, collection: any): any;
-    read(collection: any): any;
-    getById(id: any, collection: any): {
-        records: {};
-        timestamp: Date;
-    };
-    getByProperties(Object: any, collection: any): any;
-    edit(id: any, input: any, value: any, collection: any): void;
-    props(id: any, newObj: any, collection: any): void;
-    count(collection: any): {
-        recordCount: any;
-    };
+  write(input: any, collection: string): any;
+  read(collection: string): any;
+  getById(
+    id: string,
+    collection: string
+  ): {
+    records: {};
+    timestamp: Date;
+  };
+  getByProperties(Object: any, collection: string): any;
+  edit(id: string, input: string, value: string, collection: string): void;
+  props(id: string, newObj: any, collection: string): void;
+  count(collection: string): {
+    recordCount: any;
+  };
 }
