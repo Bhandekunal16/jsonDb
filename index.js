4831683a09a1f780c5490c06fa3e51bdfc7cd6ba
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-17 16:11:50 +0530

    a

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const Application = require("./src/brain");
 
 function application() {
-  return new Application().read("phone");
+  return new Application().getById("6e776c777266636a6b6a", "bank");
 }
 
-console.log(application())
-
+console.log(application());
