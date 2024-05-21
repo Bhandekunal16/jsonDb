3d5c149e61e4c3d13cee578e54b72dd339dc28a2
bhandekunal16 <bhandekunal16@gmail.com>
2024-05-21 10:34:53 +0530

    "Remove empty line and add a plus sign in Brain class"

--- src/brain.js
+++ src/brain.js
@@ -10,7 +10,7 @@ class Brain {
     try {
       const array = [];
       const addedArray = [];
-      
+
       if (input == undefined) {
         throw new Error("parameter missing Input.");
       }
