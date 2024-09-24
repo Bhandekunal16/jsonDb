e3d423bb83f9320b6bb25e4fc0328bf6d73d0e7c
bhandekunal16 <bhandekunal16@gmail.com>
2024-09-24 13:01:32 +0530

    "Refactor Vessel class to use instance variables for Heart, Response, and Logger, and update method implementations to use these instance variables."

--- src/vessels.js
+++ src/vessels.js
@@ -1,20 +1,28 @@
 const fs = require("fs");
-const heart = require("./heart");
+const Heart = require("./heart");
 const [Response, Logger] = [
   require("robotic.js/src/class/response"),
   require("robotic.js/src/interface/Logger"),
 ];
 
 class Vessel {
-  fileName = `./db.json`;
+  #heart;
+  #response;
+  #logger;
+
+  constructor() {
+    this.#heart = new Heart();
+    this.#response = new Response();
+    this.#logger = new Logger();
+  }
 
   write(input, collection) {
     try {
       this.preCreate(collection);
       fs.writeFileSync(`./${collection}.json`, JSON.stringify(input));
-      new Logger().log("your object added successfully !");
+      this.#logger.log("your object added successfully !");
     } catch (error) {
-      return new Response().error(error);
+      throw this.#response.error(error);
     }
   }
 
@@ -24,7 +32,7 @@ class Vessel {
         fs.writeFileSync(`./${collection}.json`, "[]");
       }
     } catch (error) {
-      return new Response().error(error);
+      throw this.#response.error(error);
     }
   }
 
@@ -32,17 +40,20 @@ class Vessel {
     try {
       return fs.readFileSync(`./${collection}.json`, "utf8");
     } catch (error) {
-      return new Response().error(error);
+      throw this.#response.error(error);
     }
   }
 
   writeCsv(arrayOfObjects, collection) {
-    const csvData = new heart().convertArrayOfObjectsToCsv(arrayOfObjects);
+    const csvData = this.#heart.convertArrayOfObjectsToCsv(arrayOfObjects);
     fs.writeFile(`./${collection}.csv`, csvData, "utf8", (err) => {
       if (err) {
-        new Logger().error(`Error writing CSV file at ./${collection}.csv:`, err);
+        this.#logger.error(
+          `Error writing CSV file at ./${collection}.csv:`,
+          err
+        );
       } else {
-        new Logger().log(`CSV file successfully written at ${collection}`);
+        this.#logger.log(`CSV file successfully written at ${collection}`);
       }
     });
   }
@@ -50,7 +61,7 @@ class Vessel {
   clear(collection) {
     fs.writeFile(`./${collection}.json`, "", (err) => {
       if (err) throw new Error(err);
-      new Logger().log("your data will cleared successfully!");
+      this.#logger.log("your data will cleared successfully!");
     });
   }
 }
