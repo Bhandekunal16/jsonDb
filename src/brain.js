1a936e66133135fe980e9caeae8d2f04d340a934
bhandekunal16 <bhandekunal16@gmail.com>
2024-10-10 12:36:29 +0530

    "Refactor Brain and Vessel classes: remove async/await, change file formats from binary to JSON, and move CSV writing functionality"

--- src/brain.js
+++ src/brain.js
@@ -24,7 +24,7 @@ class Brain {
     this.#response = new Response();
   }
 
-  async write(input, collection) {
+  write(input, collection) {
     try {
       const [array, addedArray] = [[], []];
       if (!input) throw new Error("parameter missing Input.");
@@ -35,12 +35,11 @@ class Brain {
         );
       this.#vessel.preCreate(collection);
       const match = this.#vessel.read(collection);
-      console.log(match);
-      if (match.length == 0) {
+      if (match.trim() === "") {
         array.push({ id: this.#uuid.vectorized(), ...input });
         this.#vessel.write(array, collection);
       } else {
-        const match = await this.#vessel.read(collection);
+        const match = JSON.parse(this.#vessel.read(collection));
         addedArray.push({ id: this.#uuid.vectorized(), ...input });
         const finalArray = [...match, ...addedArray];
         this.#vessel.write(finalArray, collection);
@@ -54,8 +53,8 @@ class Brain {
     try {
       if (!collection) throw new Error("parameter missing collection name.");
       return {
-        records: this.#vessel.read(collection),
-        length: this.#vessel.read(collection).length,
+        records: JSON.parse(this.#vessel.read(collection)),
+        length: JSON.parse(this.#vessel.read(collection)).length,
         timestamp: new Date(),
       };
     } catch (error) {
@@ -66,7 +65,7 @@ class Brain {
   getById(id, collection) {
     if (!id || !collection)
       throw new Error("Both 'id' and 'collection' parameters are required.");
-    const array = this.#vessel.read(collection);
+    const array = JSON.parse(this.#vessel.read(collection));
     return {
       records: array.filter((record) => record.id === id),
       timestamp: new Date(),
@@ -76,14 +75,14 @@ class Brain {
   getByProperties(Object, collection) {
     if (!Object || !collection)
       throw new Error(`object, collection this parameter are required.`);
-    const array = this.#vessel.read(collection);
+    const array = JSON.parse(this.#vessel.read(collection));
     return this.#heart.findSimilarObject(array, Object);
   }
 
   edit(id, input, value, collection) {
     if (!id || !input || !value || !collection)
       throw new Error("id, input, value, collection parameter are  required");
-    let array = this.#vessel.read(collection);
+    let array = JSON.parse(this.#vessel.read(collection));
     let targetObjIndex = array.findIndex((obj) => obj.id === id);
     if (targetObjIndex !== -1) {
       array[targetObjIndex][input] = value;
@@ -150,7 +149,7 @@ class Brain {
 
   delete(collection, id) {
     try {
-      const read = this.#vessel.read(collection);
+      const read = JSON.parse(this.#vessel.read(collection));
       const remove = read.filter((obj) => obj.id !== id);
       this.#vessel.write(remove, collection);
     } catch (error) {
--- src/vessels.js
+++ src/vessels.js
@@ -16,6 +16,34 @@ class Vessel {
     this.#logger = new Logger();
   }
 
+  write(input, collection) {
+    try {
+      this.preCreate(collection);
+      fs.writeFileSync(`./${collection}.json`, JSON.stringify(input));
+      this.#logger.log("your object added successfully !");
+    } catch (error) {
+      throw this.#response.error(error);
+    }
+  }
+
+  preCreate(collection) {
+    try {
+      if (!fs.existsSync(`./${collection}.json`)) {
+        fs.writeFileSync(`./${collection}.json`, "[]");
+      }
+    } catch (error) {
+      throw this.#response.error(error);
+    }
+  }
+
+  read(collection) {
+    try {
+      return fs.readFileSync(`./${collection}.json`, "utf8");
+    } catch (error) {
+      throw this.#response.error(error);
+    }
+  }
+
   writeCsv(arrayOfObjects, collection) {
     const csvData = this.#heart.convertArrayOfObjectsToCsv(arrayOfObjects);
     fs.writeFile(`./${collection}.csv`, csvData, "utf8", (err) => {
@@ -35,65 +63,6 @@ class Vessel {
       if (err) throw new Error(err);
       this.#logger.log("your data will cleared successfully!");
     });
-  }
-
-  async write(input, collection) {
-    try {
-      this.preCreate(collection);
-      fs.writeFileSync(`./${collection}.bin`, await this.binary(input));
-      this.#logger.log("your object added successfully !");
-    } catch (error) {
-      throw this.#response.error(error);
-    }
-  }
-
-  preCreate(collection) {
-    try {
-      console.log(collection);
-      if (!fs.existsSync(`./${collection}.bin`)) {
-        fs.writeFileSync(`./${collection}.bin`, this.binary([]));
-      }
-    } catch (error) {
-      throw this.#response.error(error);
-    }
-  }
-
-  binary(data) {
-    const binaryData = Buffer.from(JSON.stringify(data), "utf-8");
-    return binaryData.reduce(
-      (acc, byte) => acc + byte.toString(2).padStart(8, "0"),
-      ""
-    );
-  }
-
-  read(collection) {
-    try {
-      const data = fs.readFileSync(`./${collection}.bin`);
-      return this.read_binary(data);
-    } catch (error) {
-      throw this.#response.error(error);
-    }
-  }
-
-  read_binary(data) {
-    const binaryString = data.toString("utf-8");
-    const textFromBinary = this.binaryToText(binaryString);
-    try {
-      const jsonData = JSON.parse(textFromBinary);
-      return jsonData;
-    } catch (error) {
-      throw new Error("Failed to parse JSON from binary data.");
-    }
-  }
-
-  binaryToText(binaryString) {
-    let text = "";
-    for (let i = 0; i < binaryString.length; i += 8) {
-      const byte = binaryString.slice(i, i + 8);
-      const charCode = parseInt(byte, 2);
-      text += String.fromCharCode(charCode);
-    }
-    return text;
   }
 }
 
